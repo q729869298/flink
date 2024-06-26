204fd5ef95b52163a7bd1c9cb6baf111f84d87d0
Gabor Somogyi <gabor_somogyi2@apple.com>
2024-06-26 10:15:26 +0200

    [FLINK-36001][state] Store operator name and UID in state metadata

--- flink-runtime/src/main/java/org/apache/flink/runtime/OperatorIDPair.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/OperatorIDPair.java
@@ -31,28 +31,50 @@ public class OperatorIDPair implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private final OperatorID generatedOperatorID;
-    private final OperatorID userDefinedOperatorID;
+    @Nullable private final OperatorID userDefinedOperatorID;
+    @Nullable private final String userDefinedOperatorName;
+    @Nullable private final String userDefinedOperatorUid;
 
     private OperatorIDPair(
-            OperatorID generatedOperatorID, @Nullable OperatorID userDefinedOperatorID) {
+            OperatorID generatedOperatorID,
+            @Nullable OperatorID userDefinedOperatorID,
+            @Nullable String userDefinedOperatorName,
+            @Nullable String userDefinedOperatorUid) {
         this.generatedOperatorID = generatedOperatorID;
         this.userDefinedOperatorID = userDefinedOperatorID;
+        this.userDefinedOperatorName = userDefinedOperatorName;
+        this.userDefinedOperatorUid = userDefinedOperatorUid;
     }
 
     public static OperatorIDPair of(
-            OperatorID generatedOperatorID, @Nullable OperatorID userDefinedOperatorID) {
-        return new OperatorIDPair(generatedOperatorID, userDefinedOperatorID);
+            OperatorID generatedOperatorID,
+            @Nullable OperatorID userDefinedOperatorID,
+            @Nullable String operatorName,
+            @Nullable String operatorUid) {
+        return new OperatorIDPair(
+                generatedOperatorID, userDefinedOperatorID, operatorName, operatorUid);
     }
 
     public static OperatorIDPair generatedIDOnly(OperatorID generatedOperatorID) {
-        return new OperatorIDPair(generatedOperatorID, null);
+        return new OperatorIDPair(generatedOperatorID, null, null, null);
     }
 
     public OperatorID getGeneratedOperatorID() {
         return generatedOperatorID;
     }
 
+    @Nullable
     public Optional<OperatorID> getUserDefinedOperatorID() {
         return Optional.ofNullable(userDefinedOperatorID);
+    }
+
+    @Nullable
+    public String getUserDefinedOperatorName() {
+        return userDefinedOperatorName;
+    }
+
+    @Nullable
+    public String getUserDefinedOperatorUid() {
+        return userDefinedOperatorUid;
     }
 }
